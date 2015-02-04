c5b3faa8e90f1b9685b68b483f71ba1d96c5bafd
Bruno Antunes <sardaukar.siet@gmail.com>
2015-02-04 01:19:54 +0000

    redirect to / when logged out and landing on /logout

--- src/js/components/logout.jsx
+++ src/js/components/logout.jsx
@@ -2,6 +2,15 @@ var React = require('react'),
     auth = require('../services/auth');
 
 var Logout = React.createClass({
+
+  statics: {
+    willTransitionTo: function (transition) {
+      if ( !auth.loggedIn()) {
+        transition.redirect('/');
+      }
+    }
+  },
+
   componentDidMount: function () {
     auth.logout();
   },
